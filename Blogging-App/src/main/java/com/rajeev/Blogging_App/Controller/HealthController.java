1809618244a2682719a5bc451826a3893ccc9bd7
Rajeev <bartwalrajeev3@gamil.com>
2025-09-18 17:52:18 +0530

    Health Check Controller add n Swagger Document

--- Blogging-App/src/main/java/com/rajeev/Blogging_App/Controller/HealthController.java
+++ Blogging-App/src/main/java/com/rajeev/Blogging_App/Controller/HealthController.java
@@ -1,6 +1,7 @@
 package com.rajeev.Blogging_App.Controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/")
-@Tag(name = "Comment API's")
+@Tag(name = "Health API's")
 public class HealthController {
 
     @GetMapping("/health")
+    @Operation(summary = "used to check the health of the server...")
     public ResponseEntity<String> checkHealth(){
         return new ResponseEntity<>("health is good" , HttpStatus.OK);
     }
